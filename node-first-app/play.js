ab0332179034c13c1ab0fc2956dc48339792eb65
Dmitry Poluektov <dpoluektov@gmail.com>
2022-07-07 20:52:13 +0300

    S02: Arrow functions

--- node-first-app/play.js
+++ node-first-app/play.js
@@ -12,13 +12,33 @@ function summarizeUser(userName, userAge, userHasHobbies) {
         + userAge + 
         ', hasHobbies is ' + 
         userHasHobbies);
-}
+};
+
+const summarizeUser2 = function (userName, userAge, userHasHobbies) {
+    return (
+        'Name is ' + userName + ', age is ' + userAge + ', hasHobbies is ' + userHasHobbies);
+    };
+
+const summarizeUserArrowFunction = (userName, userAge, userHasHobbies) => {
+    return ('Name is ' + userName + ', age is ' + userAge + ', hasHobbies is ' + userHasHobbies);
+};
+
 
 console.log(summarizeUser(someName, age, hasHobbies));
+console.log(summarizeUser2(someName, age, hasHobbies));
+console.log(summarizeUserArrowFunction(someName, age, hasHobbies));
 
-// var - outdated syntax
-// new syntax - let and const instead of var
+// arrow function with two arguments
+const add = (a,b) => a + b;
 
-let myAge = 23;
-const myName = 'Jphn'; 
-myName = 'Try to change;'
+console.log(add(1,2));
+
+// arrow function for one arg - could be without ()
+const addOne = a => a + 1;
+console.log(addOne(2));
+
+// arrow function with no arguments
+const randomValue = () => 42;
+console.log(randomValue());
+
+
